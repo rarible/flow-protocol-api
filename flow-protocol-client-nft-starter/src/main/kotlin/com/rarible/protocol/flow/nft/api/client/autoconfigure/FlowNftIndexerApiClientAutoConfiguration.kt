2c80d5ec3bfde3c7577ed56dfca06fab44fef214
zolt <timochkinea@gmail.com>
2022-03-05 16:56:37 +0700

    FB-643 k8s api url

--- flow-protocol-client-nft-starter/src/main/kotlin/com/rarible/protocol/flow/nft/api/client/autoconfigure/FlowNftIndexerApiClientAutoConfiguration.kt
+++ flow-protocol-client-nft-starter/src/main/kotlin/com/rarible/protocol/flow/nft/api/client/autoconfigure/FlowNftIndexerApiClientAutoConfiguration.kt
@@ -7,8 +7,10 @@ import com.rarible.protocol.flow.nft.api.client.FlowApiServiceUriProvider
 import com.rarible.protocol.flow.nft.api.client.FlowNftIndexerApiClientFactory
 import com.rarible.protocol.flow.nft.api.client.NoopWebClientCustomizer
 import com.rarible.protocol.flow.nft.api.client.SwarmFlowNftIndexerApiServiceUriProvider
+import com.rarible.protocol.flow.nft.api.client.K8FlowNftIndexerApiServiceUriProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer
 import org.springframework.context.annotation.Bean
@@ -16,7 +18,7 @@ import org.springframework.context.annotation.Bean
 const val FLOW_NFT_INDEXER_WEB_CLIENT_CUSTOMIZER = "FLOW_NFT_INDEXER_WEB_CLIENT_CUSTOMIZER"
 
 class FlowNftIndexerApiClientAutoConfiguration(
-    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
+    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo,
 ) {
 
     @Autowired(required = false)
@@ -25,8 +27,12 @@ class FlowNftIndexerApiClientAutoConfiguration(
 
     @Bean
     @ConditionalOnMissingBean(FlowApiServiceUriProvider::class)
-    fun flowNftIndexerApiServiceUriProvider(): FlowApiServiceUriProvider {
-        return SwarmFlowNftIndexerApiServiceUriProvider(applicationEnvironmentInfo.name)
+    fun flowNftIndexerApiServiceUriProvider(@Value("\${rarible.core.client.k8s:false}") k8s: Boolean): FlowApiServiceUriProvider {
+        return if (k8s) {
+            K8FlowNftIndexerApiServiceUriProvider(applicationEnvironmentInfo.name)
+        } else {
+            SwarmFlowNftIndexerApiServiceUriProvider(applicationEnvironmentInfo.name)
+        }
     }
 
     @Bean
---
+++ flow-protocol-client-nft/src/main/kotlin/com/rarible/protocol/flow/nft/api/client/K8FlowNftIndexerApiServiceUriProvider.kt
@@ -0,0 +1,9 @@
+package com.rarible.protocol.flow.nft.api.client
+
+import java.net.URI
+
+class K8FlowNftIndexerApiServiceUriProvider(
+    private val environment: String
+): FlowApiServiceUriProvider {
+    override fun getUri(): URI = URI.create("http://flow-indexer-backend-api.${environment}-protocol:8080")
+}

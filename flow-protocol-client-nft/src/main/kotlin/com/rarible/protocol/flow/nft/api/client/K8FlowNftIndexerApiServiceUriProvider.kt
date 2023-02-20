fe142de85fefbd00b9fede2a1c215fb8b16d3c78
Artem Yakischik <a.yakischik@rarible.com>
2023-02-20 17:42:26 +0500

    URL fix

--- flow-protocol-client-nft/src/main/kotlin/com/rarible/protocol/flow/nft/api/client/K8FlowNftIndexerApiServiceUriProvider.kt
+++ flow-protocol-client-nft/src/main/kotlin/com/rarible/protocol/flow/nft/api/client/K8FlowNftIndexerApiServiceUriProvider.kt
@@ -5,5 +5,5 @@ import java.net.URI
 class K8FlowNftIndexerApiServiceUriProvider(
     private val environment: String
 ): FlowApiServiceUriProvider {
-    override fun getUri(): URI = URI.create("http://flow-indexer-api.${environment}-flow:8080")
+    override fun getUri(): URI = URI.create("http://flow-indexer-api.${environment}-protocol:8080")
 }
